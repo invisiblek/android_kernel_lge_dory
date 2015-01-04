35538b19fbd840f633bb1902da85ed8fce9d327a
arter97 <qkrwngud825@gmail.com>
2015-01-04 18:55:47 +0900

    update_f2fs.sh: regenerate

--- update_f2fs.sh
+++ update_f2fs.sh
@@ -7,6 +7,7 @@ rm -rf fs/f2fs
 git checkout f2fs/dev -- fs/f2fs
 
 git log --oneline f2fs/linux-3.10 | head -1 | awk '{print $1}' | while read patch; do git cherry-pick --no-commit $patch; done
-git cherry-pick --no-commit f144ab5a1102146b1115be75b9f7d734eb49e977
+git cherry-pick --no-commit d9d3c407cd0ee6c0f166d624d2094c9232de568b
+git cherry-pick --no-commit 86bbd59fdb8fd8835e4fd446ce11640c681860d3
 
 git status
