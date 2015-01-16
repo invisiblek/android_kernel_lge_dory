42d0de55f11744be59ae418b6c55a7da4a14476c
arter97 <qkrwngud825@gmail.com>
2015-01-16 16:12:35 +0900

    ramdisk: add hybrid support for ext4 / f2fs
    Signed-off-by: arter97 <qkrwngud825@gmail.com>

--- ramdisk/init.mount.sh
+++ ramdisk/init.mount.sh
@@ -4,6 +4,11 @@ exec 2>&1 > /dev/kmsg
 
 export PATH=/res/asset:$PATH
 
+mount -t ext4 -o rw,noatime,nodiratime,data=ordered,barrier=1,nodiscard /dev/block/platform/msm_sdcc.1/by-name/cache /cache
+mount -t f2fs -o rw,noatime,nodiratime,background_gc=on,nodiscard /dev/block/platform/msm_sdcc.1/by-name/cache /cache
+mount -t ext4 -o rw,noatime,nodiratime,data=ordered,barrier=1,nodiscard /dev/block/platform/msm_sdcc.1/by-name/userdata /data
+mount -t f2fs -o rw,noatime,nodiratime,background_gc=on,nodiscard /dev/block/platform/msm_sdcc.1/by-name/userdata /data
+
 if [ -e /data/system-bind ] ; then
 	chown 0.0 /data/system-bind
 	chmod 755 /data/system-bind
