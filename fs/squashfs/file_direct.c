5045f27a96d511c4887cd7d6f85629f20783a5de
arter97 <qkrwngud825@gmail.com>
2015-03-05 18:37:08 +0900

    Revert "fs/squashfs/file_direct.c: replace count*size kmalloc by kmalloc_array"
    This reverts commit 7fcc243f303f04272db61110e70ed2dfde565378.

--- fs/squashfs/file_direct.c
+++ fs/squashfs/file_direct.c
@@ -44,7 +44,7 @@ int squashfs_readpage_block(struct page *target_page, u64 block, int bsize)
 
 	pages = end_index - start_index + 1;
 
-	page = kmalloc_array(pages, sizeof(void *), GFP_KERNEL);
+	page = kmalloc(sizeof(void *) * pages, GFP_KERNEL);
 	if (page == NULL)
 		return res;
 
